086c6d4e66260fb6d2678fcf15e1983997a65a84
saimahidhar54 <134713245+saimahidhar54@users.noreply.github.com>
2025-07-30 21:30:08 +0530

    Added review schema to be checked by the validator

--- schema.js
+++ schema.js
@@ -12,3 +12,12 @@ module.exports.listingSchema = Joi.object(
     }).required()
     }
 );
+
+module.exports.reviewSchema = Joi.object(
+    {
+        review: Joi.object({
+            rating: Joi.number().min(1).max(5).required(),
+            comment: Joi.string().required(),
+        }).required()
+    }
+)
